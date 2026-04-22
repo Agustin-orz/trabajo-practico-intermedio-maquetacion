6ade0536ffbb7f401cd169480207bd0658162737
Agustín-orz <ao884647@gmail.com>
2026-04-21 22:51:27 -0300

    se agrega el proyecto

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ const observer = new IntersectionObserver((entries) => {
 
 document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
 
+
+const swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true, // Esto hace que la principal esté al medio
+    slidesPerView: "auto", // Esto hace que el CSS mande el ancho de 280px
+    loop: true,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
